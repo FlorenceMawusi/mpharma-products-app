05488852437d0e80ffd04320118e2799fa97bd18
FlorenceMawusi <florenceoforixyz@gmail.com>
2022-02-19 21:49:22 +0000

    fetch data functionality complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react'
 import './App.css';
+import Product from './Components/Product.js';
 
 function App() {
+
+  const [products, setProducts] = useState([]);
+
+  useEffect( () => {
+      fetch('http://www.mocky.io/v2/5c3e15e63500006e003e9795')
+      .then(
+        res => res.json()
+      )
+      .then(
+        data => console.log('data=',data.products)
+        
+      )
+      .catch(
+        err => console.log('err=',err)
+      )
+    },[])
+
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className='App'>
+      <div>
+        <h2>Add Product</h2>
+        <label>Name</label>
+        <input type='text' />
+        <br/>
+        <label>Price</label>
+        <input type='text' />
+        <br/>
+        <label>Date</label>
+        <input type='date' />
+        <button>Add</button>
+      </div>
+      <Product />
     </div>
   );
 }
---
+++ src/Components/Product.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+function Product() {
+  return (
+    <>
+      <center>
+        <h1>Product Name</h1>
+        <h3>Price</h3><p>Date</p>
+      </center>
+    </>
+  );
+}
+
+export default Product;
+
+
