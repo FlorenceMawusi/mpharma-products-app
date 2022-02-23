296114300f0a689def72a17dbd6bad8493c50a28
FlorenceMawusi <florenceoforixyz@gmail.com>
2022-02-23 16:37:54 +0000

    adding some css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, {useState, useEffect, } from 'react'
+import React, { useState, useEffect, } from 'react'
 import './App.css';
 import Product from './Components/Product.js';
 
+const LOCAL_STORAGE_KEY = 'store_products';
 
 function App() {
 
@@ -9,10 +10,17 @@ function App() {
   const [store, setStore] = useState();
   const [productname, setProductame] = useState('');
   const [price, setPrice] = useState('');
-  //const [isadd, setIsadd] = useState(True);
+  const [productId, setProductId] = useState(null);
+  const [isAdd, setIsAdd] = useState(true);
 
+  
   useEffect( () => {
-      fetch('http://www.mocky.io/v2/5c3e15e63500006e003e9795')
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(store))
+    
+  }, [store])
+
+  useEffect(() => {
+    fetch('http://www.mocky.io/v2/5c3e15e63500006e003e9795')
       .then(
         res => res.json()
       )
@@ -22,63 +30,104 @@ function App() {
           const records = data.products.map((item, index) => ({
             id: item.id,
             name: item.name,
-            pricesIds:  item?.prices.map(item=> item.id),
+            pricesIds: item?.prices.map(item => item.id),
           }));
-        
+
           const prices = data.products.map((post) => post.prices);
           const flatArray = prices.flat();
-        
+
           const state = { products: records, prices: flatArray };
-          console.log('state=',state);
+          console.log('state=', state);
+
+          const storedProducts = localStorage.getItem(LOCAL_STORAGE_KEY);
+
+          console.log('localstorage=',storedProducts );
 
           setStore(state)
+          //storedProducts?setStore(storedProducts):
+          // if (storedProducts) {
+          //   setStore(state)
+          // } else{
+          //   setStore(storedProducts)
+          // }
+          
 
 
         }
       )
       .catch(
-        err => console.log('err=',err)
+        err => console.log('err=', err)
       )
-  },[])
-  
-  
-  const addProduct = (e) => {
+  }, [])
+
+
+  const addEditProduct = (e) => {
     e.preventDefault();
-    
+
     if (productname && price) {
-      
-      const newPrice = {
-        id: store.prices.length +1,
-        price: price,
-        date: new Date().getTime().toString(),
-      }
-      console.log('price',newPrice);
-      const newProduct = {
-        id: store.products.length +1,
-        name: productname,
-        pricesIds: [newPrice.id]
-      }
-      console.log('product',newProduct);
 
-       setStore(store =>{
-         return ({
-        prices: [...store.prices, newPrice],
-        products: [...store.products, newProduct]
-      })
-       })
+      if (productId == null) {
+        const newPrice = {
+          id: store.prices.length + 1,
+          price: parseInt(price),
+          date: new Date().getTime().toString(),
+        }
+        console.log('price', newPrice);
+        const newProduct = {
+          id: store.products.length + 1,
+          name: productname,
+          pricesIds: [newPrice.id]
+        }
+        console.log('product', newProduct);
 
+        setStore(store => {
+          return ({
+            prices: [...store.prices, newPrice],
+            products: [...store.products, newProduct]
+          })
+        })
+      } else {
+        const oldProductIndex = store.products.findIndex(
+          each => productId === each.id);
+
+        const oldPriceIndex = store.prices.findIndex(
+          each => store.products[oldProductIndex].pricesIds[0] === each.id);
+
+        setStore(store => {
+
+          if(store.prices[oldPriceIndex] !== price){
+            const newPrice = {
+              id: store.prices.length + 1,
+              price: parseInt(price),
+              date: new Date().getTime().toString(),
+            }
+            store.prices.push(newPrice);
+            store.products[oldProductIndex].pricesIds.unshift(newPrice.id)
+          }
+          store.products[oldProductIndex] = {...store.products[oldProductIndex], name:productname}
+          return (
+            {
+              products: store.products,
+              prices: store.prices
+            }
+          )
+        })
+        
+      }
       setProductame('');
       setPrice('');
+      setProductId(null)
+      setIsAdd(true)
 
     } else {
       console.log('empty values')
     }
 
-  
-  }
-  
 
-  
+  }
+
+
+
 
   const handleEdit = (id) => {
     const specificProduct = store.products.find((product) => product.id === id);
@@ -87,9 +136,13 @@ function App() {
     const specificPrice = store.prices.find((price) => price.id === id);
     console.log(specificPrice)
 
+    setIsAdd(false)
+    setProductId(specificProduct.id)
     setProductame(specificProduct.name);
     setPrice(specificPrice.price)
-   // const editedProduct = {...specificProduct, ...specificProduct.name: productname}
+
+
+    // const editedProduct = {...specificProduct, ...specificProduct.name: productname}
   }
 
   const handleDelete = (id) => {
@@ -97,66 +150,85 @@ function App() {
 
     const filteredProducts = store.products.filter(product => product.id !== id);
 
-    
-    console.log('f',filteredProducts)
 
-    setStore(store =>{
+    console.log('f', filteredProducts)
+
+    setStore(store => {
       return ({
-     prices: [...store.prices],
-     products: filteredProducts
-   })
+        prices: [...store.prices],
+        products: filteredProducts
+      })
     })
 
-    
-    
-    
+
+
+
   }
 
-  const checkPrice = p => {
+  const cancel = e => {
+    e.preventDefault();
+    setProductame('');
+    setPrice('');
+    setProductId(null)
+    setIsAdd(true)
 
   }
 
   console.log(store?.prices);
   console.log(store?.products);
   return (
-    <div className='App'>
-      <h2>Add Product</h2>
-      <form onSubmit={addProduct}>
+    <div>
+
+
+
+      <div class="pageTitle title">
+        {isAdd?
+          <p>Add Product</p>:
+
+          <p>Edit Product</p>
+        } 
+      </div>
+      <form onSubmit={addEditProduct}>
         <div>
-          <input 
-            placeholder='Name' 
+          <input
+            placeholder='Name'
             type='text'
+            className="name formEntry"
             value={productname}
             onChange={e => setProductame(e.target.value)}
           />
-          
+
         </div>
-        <div>  
-          <input 
-            placeholder='Price' 
+        <div>
+          <input
+            placeholder='Price'
             type='text'
+            className="name formEntry"
             value={price}
             onChange={e => setPrice(e.target.value)}
           />
-          
+
         </div>
-        
-        <button type='submit'>Add</button>
-        
+
+        {isAdd ?
+          <button className="submit formEntry" type='submit'>Add</button> :
+          <button className="submit formEntry" type='submit'>Save</button>
+        }
+        <button className="submit formEntry" onClick={cancel}type='button'>Cancel</button>
       </form>
-      
-      
+
+      <ul className="cards">
       {store && store.products?.map(product => {
 
-        const price = store.prices?.find( price =>{
-          
-          return(price.id === product.pricesIds[0])
+        const price = store.prices?.find(price => {
+
+          return (price.id === product.pricesIds[0])
         })
-        
-        return(
-          
-          <Product 
-            key={product.id} 
+
+        return (
+
+          <Product
+            key={product.id}
             product={product}
             price={price}
             handleEdit={handleEdit}
@@ -165,7 +237,8 @@ function App() {
         )
       })
       }
-      
+      </ul>
+
     </div>
   );
 }
--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,16 +1,32 @@
 import React from 'react';
+import '../App.css';
 
 
 function Product({product, price, handleDelete, handleEdit}) {
   return (
     <>
-    
-      <center>
-        <h1>{product.name}</h1>
-        <h3>{price.price} cedis</h3>
-        <button onClick={() => handleEdit(product.id)}>Edit</button>
-        <button onClick={() => handleDelete(product.id)}>Delete</button>
-      </center>
+      <ul className="cards">
+        <li>
+          <a href="" className="card">
+            <img src="https://i.imgur.com/2DhmtJ4.jpg" className="card__image" alt="" />
+            <div className="card__overlay">        
+              <div className="card__header">
+                <svg className="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                 
+                <div className="card__header-text">
+                  <h3 className="card__title">{product.name}</h3>
+                  <span className="card__status">{price.price} cedis</span>
+                </div>
+              </div>
+              <div>
+                <button className="product_button" onClick={() => handleEdit(product.id)}>Edit</button>
+                <button className="product_button" onClick={() => handleDelete(product.id)}>Delete</button>
+              </div>
+              
+            </div>
+          </a>
+      </li>  
+    </ul>
+      
     </>
   );
 }
